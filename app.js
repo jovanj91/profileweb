9d9c5dd4c7c4b335421e3c469bbde46efdfbb4e2
Jovan Josafat <jovan.josafat@gmail.com>
2023-10-23 21:12:33 +0700

    Initial commit

--- app.js
+++ app.js
@@ -7,13 +7,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/')));
 
 
-// const HOST = '0.0.0.0';
-// const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 8080;
 
-// app.listen(PORT, HOST)
-// console.log(`Running on http://${HOST}:${PORT}`);
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
